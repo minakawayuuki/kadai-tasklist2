6215d6dc74bce53ac314efd172d04d0bff26ca7c
minakawayuuki <minayuki0204@icloud.com>
2020-09-18 18:14:16 +0900

    Add NewServlet.java

--- src/models/Task.java
+++ src/models/Task.java
@@ -34,6 +34,9 @@ public class Task {
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
+
     public Integer getId() {
         return id;
     }
@@ -65,5 +68,15 @@ public class Task {
     public void setCreated_at(Timestamp created_at) {
         this.created_at = created_at;
     }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+
 
 }
